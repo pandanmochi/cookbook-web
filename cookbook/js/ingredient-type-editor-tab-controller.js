8d9f8cd188703183aa3bc504213fbe2066ed9ce4
pandanmochi <to.un.thi@gmail.com>
2024-06-21 11:44:09 +0200

    add recipe view

--- cookbook/js/ingredient-type-editor-tab-controller.js
+++ cookbook/js/ingredient-type-editor-tab-controller.js
@@ -23,21 +23,48 @@ class IngredientTypeEditorTabController extends Controller {
   /**
    * Handles that activity has changed from false to true.
    */
-  async processActivated() {
+  processActivated() {
     this.clear();
     this.top.querySelector("nav.tabs>button.ingredient-type-editor").classList.add("active");
 
     const viewTemplate = document.querySelector("template.ingredient-types-view");
     const viewSection = viewTemplate.content.firstElementChild.cloneNode(true);
 
-    const editorTemplate = document.querySelector("template.ingredient-type-editor");
-    const editorSection = editorTemplate.content.firstElementChild.cloneNode(true);
-
     this.center.append(viewSection);
-    this.center.append(editorSection);
-    this.center.querySelector("section.ingredient-type-editor").classList.add("hidden");
+
+    // register basic event listeners
+    const createIngredientButton = viewSection.querySelector("button.create");
+    createIngredientButton.addEventListener("click", (event) =>
+      this.#displayEditIngredient({ identity: 0, avatar: { identity: 1 } })
+    );
 
     this.#displayIngredientTypes();
+    }
+    
+  #displayEditIngredient(ingredient) {
+    this.center.querySelector("section.ingredient-types-view").classList.add("hidden");
+    const editorTemplate = document.querySelector("template.ingredient-type-editor");
+    const editorSection = editorTemplate.content.firstElementChild.cloneNode(true);
+    this.center.append(editorSection);
+
+    editorSection.querySelector("img.avatar").src = "/services/documents/" + ingredient.avatar.identity;
+    editorSection.querySelector("input.alias").value = ingredient.alias || "";
+    editorSection.querySelector("select.restriction").value = ingredient.restriction || "NONE";
+    editorSection.querySelector("textarea.description").value = ingredient.description || "";
+      
+    const submitButton = editorSection.querySelector("button.submit");
+    submitButton.addEventListener("click", (event) => this.saveIngredient(ingredient));
+  
+    const cancelButton = editorSection.querySelector("button.cancel");
+    cancelButton.addEventListener("click", (event) => this.cancelEditor());
+
+    const avatarImageElement = editorSection.querySelector("img.avatar");
+    avatarImageElement.addEventListener("dragover", (event) =>
+      this.validateAvatarTransfer(event.dataTransfer)
+    );
+    avatarImageElement.addEventListener("drop", (event) =>
+      this.processSubmitAvatar(ingredient, event.dataTransfer.files[0])
+    );
   }
 
   async #displayIngredientTypes() {
@@ -61,20 +88,8 @@ class IngredientTypeEditorTabController extends Controller {
       viewRowSection.querySelector("td.modified").innerText = new Date(ingredient.modified).toDateString();
 
       const accessEditButton = viewRowSection.querySelector("button.access");
-      accessEditButton.addEventListener("click", (event) => this.editIngredient(ingredient));
+      accessEditButton.addEventListener("click", (event) => this.#displayEditIngredient(ingredient));
     }
-
-    // register basic event listeners
-    const createIngredientButton = viewSection.querySelector("button.create");
-    createIngredientButton.addEventListener("click", (event) =>
-      this.editIngredient({ identity: 0, avatar: { identity: 1 } })
-    );
-
-    const submitButton = editorSection.querySelector("button.submit");
-    submitButton.addEventListener("click", (event) => this.saveIngredient(ingr));
-
-    const cancelButton = editorSection.querySelector("button.cancel");
-    cancelButton.addEventListener("click", (event) => this.cancelEditor());
   }
 
   async saveIngredient(ingredient) {
@@ -90,19 +105,8 @@ class IngredientTypeEditorTabController extends Controller {
       const response = await fetch(resource, { method: "POST", headers: headers, body: JSON.stringify(ingredient) });
       if (!response.ok) throw new Error("HTTP " + response.status + " " + response.statusText);
       this.#messageElement.value = "ok.";
-
-      // append new ingredient to list
-      const viewRowTemplate = document.querySelector("template.ingredient-types-view-row");
-      const viewRowSection = viewRowTemplate.content.firstElementChild.cloneNode(true);
-
-      this.center.querySelector("table.ingredient-types").append(viewRowSection);
-      viewRowSection.querySelector("td.alias").innerText = alias;
-      viewRowSection.querySelector("td.restriction").innerText = RESTRICTION[restriction];
-      viewRowSection.querySelector("td.modified").innerText = new Date().toDateString();
-
-      /* const accessEditButton = viewRowSection.querySelector("button.access");
-      accessEditButton.addEventListener("click", (event) => this.ingredientEdit());
-      accessEditButton.ingredient = ingredient; */
+      
+      this.#displayIngredientTypes();
 
       this.cancelEditor();
     } catch (error) {
@@ -135,13 +139,9 @@ class IngredientTypeEditorTabController extends Controller {
   }
 
   cancelEditor() {
-    this.#displayIngredientTypes();
     this.center.querySelector("section.ingredient-types-view").classList.remove("hidden");
     const editorSection = this.center.querySelector("section.ingredient-type-editor");
-    editorSection.classList.add("hidden")
-    editorSection.querySelector("input").value = "";
-    editorSection.querySelector("textarea").value = "";
-    editorSection.querySelector("select").value = "NONE";
+    editorSection.remove();
   }
 
   editIngredient(ingredient) {
@@ -153,7 +153,48 @@ class IngredientTypeEditorTabController extends Controller {
     editor.querySelector("select.restriction").value = ingredient.restriction || "NONE";
     editor.querySelector("textarea.description").value = ingredient.description || "";
   }
+
+  async validateAvatarTransfer(dataTransfer) {
+    const dataTransferItem = dataTransfer.items[0];
+    dataTransfer.dropEffect =
+      dataTransferItem.kind === "file" && dataTransferItem.type.startsWith("image/")
+        ? "copy"
+        : "none";
+  }
+
+  async processSubmitAvatar(ingredient, avatarFile) {
+    try {
+      
+      if (!avatarFile) throw new ReferenceError("no avatar file dropped!");
+      if (!avatarFile.type.startsWith("image/"))
+        throw new RangeError("illegal avatar file " + avatarFile.name + "!");
+      const sessionOwner = this.sharedProperties["session-owner"];
+
+      const query = new URLSearchParams();
+      query.set("description", avatarFile.name);
+      const resource = "/services/documents?" + query.toString();
+
+      const headers = { Accept: "text/plain", "Content-Type": avatarFile.type };
+      const response = await fetch(resource, {
+        method: "POST",
+        headers: headers,
+        body: avatarFile,
+      });
+      if (!response.ok) throw new Error("HTTP " + response.status + " " + response.statusText);
+      ingredient.avatar.identity = parseInt(await response.text());
+
+      this.center.querySelector("section.ingredient-type-editor img.avatar").src =
+        "/services/documents/" + ingredient.avatar.identity;
+
+      this.#messageElement.value = "ok.";
+    } catch (error) {
+      this.#messageElement.value = error.toString();
+      console.error(error);
+    }
+  }
 }
+
+
 
 /*
  * Registers an event handler for the browser window's load event.
--- cookbook/js/recipe-view-tab-controller.js
+++ cookbook/js/recipe-view-tab-controller.js
@@ -1,4 +1,5 @@
 import Controller from "../../tool/controller.js";
+import { RESTRICTION, CATEGORY } from "./enums.js";
 
 /**
  * Skeleton for tab controller type.
@@ -22,7 +23,7 @@ class RecipeViewTabController extends Controller {
   /**
    * Handles that activity has changed from false to true.
    */
-  processActivated() {
+  async processActivated() {
     this.clear();
     this.top.querySelector("nav.tabs>button.recipe-view").classList.add("active");
 
@@ -36,17 +37,113 @@ class RecipeViewTabController extends Controller {
     const recipeViewRowSection = recipeViewRowTemplate.content.firstElementChild.cloneNode(true);
 
     this.center.append(recipeViewQuerySection);
-    this.center.append(recipesViewSection);
-    recipesViewSection.firstElementChild.querySelector("tbody").append(recipeViewRowSection);
+    //this.center.append(recipesViewSection);
+    //recipesViewSection.firstElementChild.querySelector("tbody").append(recipeViewRowSection);
 
     const recipesViewIngredientRowTemplate = document.querySelector("template.recipe-view-ingredient-row");
     const recipesViewIngredientRowSection = recipesViewIngredientRowTemplate.content.firstElementChild.cloneNode(true);
 
     // register basic event listeners
 
-    const recipeButton = recipeViewRowSection.querySelector("button.access");
-    recipeButton.addEventListener("click", (event) => this.openRecipe());
+    const searchButton = recipeViewQuerySection.querySelector("button.query");
+    searchButton.addEventListener("click", (event) => this.showSearchResults())
+  }
+
+  async showSearchResults(){
+    const oldRecipeViewSection = this.center.querySelector("section.recipes-view");
+    if(oldRecipeViewSection) oldRecipeViewSection.remove();
+
+    const section = this.center.querySelector("section.recipes-view-query");
+    
+    const recipesViewTemplate = document.querySelector("template.recipes-view");
+    const recipesViewSection = recipesViewTemplate.content.firstElementChild.cloneNode(true);
+    this.center.append(recipesViewSection);
+
+    const tableBody = recipesViewSection.querySelector("table.recipes>tbody");
+    tableBody.innerHTML = "";
+    
+    const searchQuery = {
+      "restriction": section.querySelector("select.restriction").value,
+      "title": section.querySelector("input.title").value,
+      "category": section.querySelector("select.category").value,
+      "ingredientCount": section.querySelector("input.min-ingredient-count").value
+      }
       
+    const recipes = await this.queryRecipes();
+    const filteredRecipes = this.filterRecipes(searchQuery, recipes);
+    console.log(filteredRecipes);
+
+    for (const recipe of filteredRecipes) {
+      const recipeViewRowTemplate = document.querySelector("template.recipes-view-row");
+      const recipeViewRowSection = recipeViewRowTemplate.content.firstElementChild.cloneNode(true);
+      tableBody.append(recipeViewRowSection);
+
+      recipeViewRowSection.querySelector("img.avatar").src = "/services/documents/" + recipe.avatar.identity;
+      recipeViewRowSection.querySelector("td.title").innerText = recipe.title;
+      recipeViewRowSection.querySelector("td.restriction").innerText = RESTRICTION[recipe.restriction];
+      recipeViewRowSection.querySelector("td.category").innerText = CATEGORY[recipe.category];
+      recipeViewRowSection.querySelector("td.ingredient-count").innerText = recipe.ingredientCount;
+      recipeViewRowSection.querySelector("td.modified").innerText = new Date(recipe.modified).toDateString();
+
+      const recipeButton = recipeViewRowSection.querySelector("button.access");
+      recipeButton.addEventListener("click", (event) => this.openRecipe());
+    }
+  }
+
+  filterRecipes(searchQuery, recipes) {
+    let filteredRecipes = [];
+
+    let restrictions = [
+      "NONE", // 0
+      "PESCATARIAN", // 1
+      "LACTO_OVO_VEGETARIAN", // 2
+      "LACTO_VEGETARIAN", // 3
+      "VEGAN" // 4
+    ];
+
+    for(const recipe of recipes){
+      const restrictionSearchIndex = restrictions.indexOf(searchQuery.restriction);
+      const restrictionRecipeIndex = restrictions.indexOf(recipe.restriction);
+      const searchQueryTitleString = String(searchQuery.title).toLowerCase();
+      const titleString = String(recipe.title.toLowerCase());
+
+      // console.log(searchQuery.category)
+      // console.log("count: " + recipe.ingredientCount >= searchQuery.ingredientCount)
+      // console.log("category: " + recipe.category === searchQuery.category);
+      // console.log("category: " + searchQuery.category === "");
+      // console.log("searchQueryTitleString: " + searchQueryTitleString + "; titleString: " + titleString)
+      // console.log("restrictionRecipeIndex: " + restrictionRecipeIndex + "; restrictionSearchIndex: " + restrictionSearchIndex)
+
+      if(recipe.ingredientCount >= searchQuery.ingredientCount && 
+        (searchQuery.category === "" || recipe.category == searchQuery.category) &&
+        (searchQuery.title == undefined || titleString.includes(searchQueryTitleString)) &&
+        restrictionRecipeIndex >= restrictionSearchIndex)
+        {
+          filteredRecipes.push(recipe);
+      }
+    }
+
+    return filteredRecipes;
+  }
+
+  async queryRecipes() {
+    try {
+
+      const headers = { Accept: "application/json" };
+      const resource = "/services/recipes";
+
+      const response = await fetch(resource, { method: "GET", headers: headers });
+
+      if (!response.ok) throw new Error("HTTP " + response.status + " " + response.statusText);
+      this.#messageElement.value = "fetched ingredients.";
+
+      const recipes = await response.json();
+
+      return recipes;
+    } catch (error) {
+      this.#messageElement.value = error.toString();
+      console.error(error);
+    }
   }
 
   openRecipe(){
